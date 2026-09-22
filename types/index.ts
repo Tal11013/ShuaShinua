59d5fd0575bd6ec5db318d9fb02083c5d394368e
Tal11013 <tal.glikin@live.biu.ac.il>
2026-09-22 16:12:43 +0300

    initial models
    Signed-off-by: Tal11013 <tal.glikin@live.biu.ac.il>

---
+++ types/index.ts
@@ -0,0 +1,7 @@
+export * from "./common";
+export * from "./item";
+export * from "./room";
+export * from "./packing";
+export * from "./moving";
+export * from "./action";
+
---
+++ types/moving.ts
@@ -0,0 +1,22 @@
+import type { UUID } from "./common";
+
+export enum MovingType {
+  TRACK = "TRACK",
+  CAR = "CAR",
+}
+
+export enum MovingUnitStatus {
+  WAITING_FOR_MOVING = "WAITING_FOR_MOVING",
+  LOADING_PROCESS = "LOADING_PROCESS",
+  ON_WAY = "ON_WAY",
+  UNLOADED_AT_DESTINATION = "UNLOADED_AT_DESTINATION",
+  CLOSED = "CLOSED",
+}
+
+export interface MovingUnit {
+  moving_id: UUID;
+  moving_type: MovingType;
+  moving_status: MovingUnitStatus;
+  moving_date: Date;
+}
+
